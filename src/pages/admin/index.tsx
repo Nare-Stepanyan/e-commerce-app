c254191442c5e2a18faaf45976680e6b5dc915d7
Admin <admin@Nare-Preezma.local>
2024-03-16 11:59:13 +0400

    add admin page functionality

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,8 +2,14 @@ import React, { useState } from "react";
 import styles from "./admin.module.scss";
 import { Product } from "../../type";
 import Button from "../../components/button";
+import { useAppDispatch } from "../../components/app/hook";
+import { createProduct } from "../../store/products/actions";
+import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
   const [product, setProduct] = useState<Product>({
     title: "",
     count: 0,
@@ -19,66 +25,82 @@ const Admin = () => {
       [name]: value,
     }));
   };
-  const addNewProduct = () => {
-    console.log("add");
+  const addNewProduct = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = await dispatch(createProduct(product));
+    if (data) {
+      setProduct({
+        title: "",
+        count: 0,
+        description: "",
+        imageUrl: "",
+        price: 0,
+      });
+      navigate("/");
+    } else {
+      setError("Something went wrong");
+    }
   };
 
   return (
     <div className={styles.container}>
       <h2>Add New Product</h2>
-      <label>
-        Title:
-        <input
-          type="text"
-          name="title"
-          required
-          value={product.title}
-          onChange={handleValues}
-        />
-      </label>
-      <label>
-        Description:
-        <input
-          type="text"
-          name="description"
-          required
-          value={product.description}
-          onChange={handleValues}
-        />
-      </label>
-      <label>
-        Count:
-        <input
-          type="number"
-          name="count"
-          placeholder="Count"
-          required
-          value={product.count}
-          onChange={handleValues}
-        />
-      </label>
-      <label>
-        Price:
-        <input
-          type="number"
-          name="price"
-          placeholder="Price"
-          required
-          value={product.price}
-          onChange={handleValues}
-        />
-      </label>
-      <label>
-        Image url:
-        <input
-          type="imageUrl"
-          name="title"
-          required
-          value={product.imageUrl}
-          onChange={handleValues}
-        />
-      </label>
-      <Button label="Add" onClick={addNewProduct} />
+      <form onSubmit={addNewProduct}>
+        <label>
+          Title:
+          <input
+            type="text"
+            name="title"
+            required
+            value={product.title}
+            onChange={handleValues}
+          />
+        </label>
+        <label>
+          Description:
+          <input
+            type="text"
+            name="description"
+            required
+            value={product.description}
+            onChange={handleValues}
+          />
+        </label>
+        <label>
+          Count:
+          <input
+            type="number"
+            name="count"
+            placeholder="Count"
+            required
+            value={product.count}
+            onChange={handleValues}
+          />
+        </label>
+        <label>
+          Price:
+          <input
+            type="number"
+            name="price"
+            placeholder="Price"
+            required
+            value={product.price}
+            onChange={handleValues}
+          />
+        </label>
+        <label>
+          Image url:
+          <input
+            type="imageUrl"
+            name="imageUrl"
+            required
+            value={product.imageUrl}
+            onChange={handleValues}
+          />
+        </label>
+        <Button label="Add" />
+        {error && error}
+      </form>
     </div>
   );
 };
