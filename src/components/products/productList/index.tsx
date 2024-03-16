283ed5d345242f367eb38c911b00c1b211654abf
Admin <admin@Nare-Preezma.local>
2024-03-16 22:54:40 +0400

    add search component

--- src/components/products/productList/index.tsx
+++ src/components/products/productList/index.tsx
@@ -1,15 +1,22 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import styles from "./productList.module.scss";
 import ProductItem from "../productItem";
 import { Product } from "../../../type";
+import Search from "../../search";
 
 type ProductListProps = {
   products: Product[] | null;
 };
 
 const ProductList: FC<ProductListProps> = ({ products }) => {
+  const [search, setSearch] = useState("");
+
   return (
     <div className={styles["product-list"]} id="product">
+      <div>
+        <Search value={search} onChange={(e) => setSearch(e.target.value)} />
+      </div>
+
       <div className={styles.list}>
         {products?.length === 0 ? (
           <p>No product found.</p>
